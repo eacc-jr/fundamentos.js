fc3fea4a0ae5bc53fcdea15c62ac2504caad3dd8
Eduardo Castagnari <111157689+eacc-jr@users.noreply.github.com>
2024-01-02 11:36:02 -0300

    Exemplos truthy e conversões

---
+++ conversoes.js
@@ -0,0 +1,18 @@
+// tipo de dado
+//booleanos
+
+//conversão implicita
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString);
+console.log(numero == numeroString);
+
+console.log(numeroString + numero);
+
+//conversão explicita
+
+//Number()
+//String()
+
+console.log(Number(numeroString) + numero);
